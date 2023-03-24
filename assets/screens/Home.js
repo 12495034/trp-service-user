b1fd3f9471ca8fa16f11adb4e72cd3073ba6ca00
12495034 <gdavis02@qub.ac.uk>
2023-03-24 20:02:09 +0000

    home screen content update

--- assets/screens/Home.js
+++ assets/screens/Home.js
@@ -8,20 +8,11 @@ export default function Home() {
 
     const { user, role, status} = useContext(AuthContext);
     const [expandedBooking, setExpandedBooking] = useState(false);
-    // const [expandedAtClinic, setExpandedAtClinic] = useState(false);
-
-    console.log("role:", role)
-    console.log("status:", status)
 
     //state set to close one accordion if another is opened to limit information overload
     const handlePressBooking = () => {
         setExpandedBooking(!expandedBooking)
-        setExpandedAtClinic(false)
     };
-    // const handlePressAtClinic = () => {
-    //     setExpandedAtClinic(!expandedAtClinic)
-    //     setExpandedBooking(false)
-    // };
 
     return (
         <View style={HomeStyles.body}>
@@ -40,7 +31,7 @@ export default function Home() {
                         left={props => <List.Icon {...props} color='green' icon="plus-circle-outline" />}
                     />
                     <List.Item
-                        title="A quick question"
+                        title="Lets get started..."
                         descriptionNumberOfLines={3}
                         description="Answer a quick question so we can determine if a test is suitible for you"
                         left={props => <List.Icon {...props} color='green' icon="help-circle-outline" />}
@@ -52,53 +43,24 @@ export default function Home() {
                         left={props => <List.Icon {...props} color='green' icon="card-search-outline" />}
                     />
                     <List.Item
-                        title="Pick a slot and confirm"
+                        title="Clinic Information"
                         descriptionNumberOfLines={3}
-                        description="choose a suitible time slot in a clinic. You will have 60 seconds to confirm the appointment or the slot will be released"
+                        description="choose a suitible time slot at the clinic of your choice and confirm."
                         left={props => <List.Icon {...props} color='green' icon="clock-edit-outline" />}
+                    />
+                    <List.Item
+                        title="Appointment Confirmation"
+                        descriptionNumberOfLines={4}
+                        description="You will have 60 seconds to confirm you are happy with the appointment details. After that time the slot is released and the booking process will restart."
+                        left={props => <List.Icon {...props} color='green' icon="account-check-outline" />}
                     />
                     <List.Item
                         title="View appointments"
                         descriptionNumberOfLines={3}
                         description="Any confirmed bookings and your booking history can be found on the appointments tab"
-                        left={props => <List.Icon {...props} color='green' icon="format-list-bulleted-square" />}
-                    />
-                </List.Accordion>
-                {/* <List.Accordion
-                    title="What to do at the clinic"
-                    expanded={expandedAtClinic}
-                    onPress={handlePressAtClinic}>
-                    <List.Item
-                        title="View appointments"
-                        descriptionNumberOfLines={3}
-                        description="Access your booked appointments using the appointments tab"
                         left={props => <List.Icon {...props} color='green' icon="calendar" />}
                     />
-                    <List.Item
-                        title="Check in"
-                        descriptionNumberOfLines={3}
-                        description="Press the red user icon to let the clinic know you have arrived"
-                        left={props => <List.Icon {...props} color='green' icon="account-check" />}
-                    />
-                    <List.Item
-                        title="Called"
-                        descriptionNumberOfLines={3}
-                        description="When called for your test the bell icon will turn purple and the testers name will be shown"
-                        left={props => <List.Icon {...props} color='purple' icon="bell" />}
-                    />
-                    <List.Item
-                        title="Test complete"
-                        descriptionNumberOfLines={3}
-                        description="When your test is complete the thumb icon will turn green"
-                        left={props => <List.Icon {...props} color='green' icon="thumb-up" />}
-                    />
-                    <List.Item
-                        title="Cancellations"
-                        descriptionNumberOfLines={3}
-                        description="You can cancel an appointment with a minimum of24hrs. Press and hold the appointment to cancel."
-                        left={props => <List.Icon {...props} color='red' icon="cancel" />}
-                    />
-                </List.Accordion> */}
+                </List.Accordion>
             </ScrollView>
         </View>
 
